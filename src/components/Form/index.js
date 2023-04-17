2b4994a77cf05605b006087aff3cfb2b64641181
mqsoares <mqseraos@gmail.com>
2023-04-17 11:06:27 -0300

    Create component ListSelect]

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,20 @@
 import { Input } from "../InputsTxt"
+import { ListSelect } from "../ListSelect"
 import "./Form.css"
 
 export const Form = () => {
+    
+    const times = [
+        "",
+        "Programação",
+        "Fornt-End",
+        "Data Science",
+        "Devops",
+        "UX e Design",
+        "Mobile",
+        "Ivovação e Gestão"
+    ]
+
     return (
         <section className="form">
             <h3>Preencha os dados para criar o card do colaborador.</h3>
@@ -9,6 +22,7 @@ export const Form = () => {
             <Input name="Nome" placeholder="Digite seu nome" />
             <Input name="Cargo" placeholder="Digite seu cargo" />
             <Input name="Imagem" placeholder="Informe o endereço da imagem" />
+            <ListSelect name="time" items={times} />
             </form>
         </section>
     )
